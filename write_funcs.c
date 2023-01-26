76ef20866400ab23aeca80789d799179ca668e2b
Enoch Amankwah Boahen <ea.boahen2015@gmail.com>
2023-01-26 00:54:55 +0000

    enoch betty corrections

--- write_funcs.c
+++ write_funcs.c
@@ -12,6 +12,7 @@ int _putchar(char c)
 {
 	static char buf[1024];
 	static int i;
+
 	if (c == -1 || i >= 1024)
 	{
 		write(1, &buf, i);
@@ -32,6 +33,7 @@ int _putchar(char c)
 int _puts(char *str)
 {
 	register int i;
+
 	for (i = 0; str[i] != '\0'; i++)
 		_putchar(str[i]);
 	return (i);
