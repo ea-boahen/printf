5bba4155266b02b3a4c5ccdf1cbeb79c645b7e34
AnielaAndoh <anielaandoh.com>
2023-01-25 17:06:36 -0800

    printf d i

--- get_print.c
+++ get_print.c
@@ -13,10 +13,10 @@
 int (*get_print(char s))(va_list, flags_t *)
 {
 	ph func_arr[] = {
-		/*{'i', print_int},*/
+		{'i', print_int},
 		{'s', print_string},
-		{'c', print_char} /*,
-		*{'d', print_int},
+		{'c', print_char},
+		{'d', print_int} /*,
 		*{'u', print_unsigned},
 		*{'x', print_hex},
 		*{'X', print_hex_big},
